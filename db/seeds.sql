51b649a7366814fc84d70efad2f4f3148368cedb
Diane Galiano <dianegaliano09@gmail.com>
2021-10-14 20:53:21 -0400

    added insert into values to seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,15 @@
+INSERT INTO department (name)
+VALUES ("Engineering"),
+       ("Finance"),
+       ("Legal"),
+       ("Sales");
+
+INSERT INTO role (name, department)
+VALUES ("Sales Lead", 4),
+       ("Salesperson", 4),
+       ("Lead Engineer", 1),
+       ("Software Engineer", 1),
+       ("Account Manager", 2),
+       ("Accountant", 2),
+       ("Legal Team Lead", 3);
+       ("Lawyer", 3);       
