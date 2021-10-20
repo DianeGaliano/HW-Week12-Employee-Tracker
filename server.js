e14da72308e11e8eb7b5d722dd54c73ee588488e
Diane Galiano <dianegaliano09@gmail.com>
2021-10-20 18:43:31 -0500

    deleted index.js and added the prompt functions to server.js

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
 const express = require('express');
 
 // Import the connection object
 const sequelize = require('./config/connection');
+const { update } = require("lodash");
+const { connect } = require("http2");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -26,4 +30,39 @@ db.query('SELECT * FROM students', function (err, results) {
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
   });
-  
+
+inquirer
+    .promt ([
+        {
+            type: "checkbox",
+            name: "choice",
+            message: "What would you like to do?",
+            choices: [
+                "View All Employees",
+                "Add Employee",
+                "Update Employee Role",
+                "View all roles",
+                "View all departments",
+                "Add department",
+                "Quit",
+            ]        
+        },
+    ])
+
+    .then(function(answer){
+        if (answer.action === "View all employees") {
+          viewEmployee();
+        }else if (answer.action === "Add Employee") {
+          addEmployee();
+        }else if (answer.action === "Update Employee Role") {
+          updateRole();
+        }else if (answer.action === "View all Roles") {
+          viewRoles();
+        }else if (answer.action === "View all Departments") {
+          viewDempartments();
+        }else if (answer.action === "Add Department") {
+          addDepartment();
+        }else if (answer.action == "Quit") {
+          connection.end();
+        }
+    });
